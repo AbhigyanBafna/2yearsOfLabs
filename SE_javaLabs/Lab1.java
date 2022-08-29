e1fd5fd021879bd468a4dd28185c62095a84053e
AbhigyanBafna <101444239+AbhigyanBafna@users.noreply.github.com>
2022-08-29 15:04:29 +0530

    Codes done in First Lab of Java

---
+++ SE_javaLabs/Lab1.java
@@ -0,0 +1,81 @@
+import java.util.*;
+
+public class Lab1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        //Factorial
+        System.out.print("Enter a number for its Factorial: ");
+        int n = sc.nextInt();
+        System.out.println("");
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact = fact * i;
+        }
+        System.out.println("Factorial is " + fact);
+        System.out.println("");
+        System.out.println("");
+      
+      
+        //Armstrong
+        System.out.print("Enter a number to check for Armstrong: ");
+        int arm = sc.nextInt();
+        System.out.println("");
+		    int rem, sum = 0 , flag = n;
+		    while (arm>0) {
+    			rem = arm%10;
+    			sum = sum + (rem*rem*rem);
+    			arm=arm/10;
+    		}
+    		if (sum==flag){
+      			System.out.println(flag + " is an Armstrong number");
+      		}
+    		else{
+    			System.out.println(flag + " is not an Armstrong number");
+    		}
+    		System.out.println("");
+    		System.out.println("");
+		
+    
+        //Quadratic Eqn
+        double r1, r2;
+        System.out.print("Enter value for a in Eqn : ");
+        double a = sc.nextInt();
+        System.out.println("");
+        System.out.print("Enter value for b in Eqn : ");
+        double b = sc.nextInt();
+        System.out.println("");
+        System.out.print("Enter value for c in Eqn : ");
+        double c = sc.nextInt();
+        System.out.println("");
+        r1 = -b + Math.sqrt((b * b - 4 * a * c))/(2*a);
+        r2 = -b - Math.sqrt((b * b - 4 * a * c))/(2*a);
+        System.out.println("The algebraic roots of the equation are " + r1 + " and " + r2 + ".");
+        
+        
+        /*
+        int n, i, j;
+        System.out.println("Enter a number of rows: ");
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < n - i; j++) {
+                System.out.print("  ");
+            }
+            for (j = 0; j <= i; j++) {
+                System.out.print(fact(i) / (fact(i - j) * fact(j)) + "   ");
+            }
+            System.out.print("\n");
+        }
+    }
+    private static int fact(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        return n * fact(n - 1);
+    }
+    */
+        
+        
+    }
+}  
