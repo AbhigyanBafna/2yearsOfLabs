69fe73d956365cfbcfeedb220aa4a7f894b80f07
AbhigyanBafna <101444239+AbhigyanBafna@users.noreply.github.com>
2023-03-28 12:30:02 +0530

    Update and rename assign5.tcl to distanceVector.tcl

--- deprecated/SY/Networks/distanceVector.tcl
+++ deprecated/SY/Networks/distanceVector.tcl
@@ -33,23 +33,18 @@ $ns cost $n1 $n2 3
 $ns cost $n2 $n3 11
 
 set tcp [new Agent/TCP]
-$ns attach-agent $n0 $tcp
+$ns attach-agent $n1 $tcp
 set ftp [new Application/FTP]
 $ftp attach-agent $tcp
 set sink [new Agent/TCPSink]
 $ns attach-agent $n3 $sink
-set udp [new Agent/UDP]
-$ns attach-agent $n2 $udp
-set cbr [new Application/Traffic/CBR]
-$cbr attach-agent $udp
-set null [new Agent/Null]
-$ns attach-agent $n3 $null
+
 $ns connect $tcp $sink
-$ns connect $udp $null
+
 $ns rtmodel-at 1.0 down $n1 $n3
 $ns rtmodel-at 2.0 up $n1 $n3
 $ns rtproto DV
+
 $ns at 0.0 "$ftp start"
-$ns at 0.0 "$cbr start"
 $ns at 5.0 "finish"
 $ns run
